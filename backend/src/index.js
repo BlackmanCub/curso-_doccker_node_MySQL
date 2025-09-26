77a174976c253364180349d2d035aa71b7391807
BlackmanCub <pedroenriquerhrh@gmail.com>
2025-09-26 00:07:59 -0500

    se creo la variable de test

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,14 +1,19 @@
+require('dotenv').config();
+
+
 const express = require('express');
 const app = express();
+const port=process.env.PORT || 3000;
+const jwtSecret=process.env.JWT_SECRET
 
 app.use(express.json());
+console.log(jwtSecret);
+console.log(port);
 
 app.get("/", (request, response) => {
     response.send('API backend funcionando'); // corregido res -> response
 });
 
-const PORT = process.env.PORT || 3000; // corregido Port -> PORT
-
-app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`); // backticks
+app.listen(port, () => {
+    console.log(`Servidor escuchando en el puerto ${port}`); // backticks
 });
